ad5d2cc5ed78a10a64a0cad031e3248ae52940c0
Keith Horrocks <KeithJH@users.noreply.github.com>
2025-03-03 17:25:39 -0800

    Further split main file
    Split `App` class out

---
+++ App.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "kinematics.h"
+
+class App
+{
+  public:
+	App(const float width, const float height, const size_t initialNumBodies);
+
+	void Update();
+
+	/// Top level draw function for the entire scene.
+	/// Includes: Background, FPS counter, statistics and `Simulation` contents.
+	void DrawFrame() const;
+
+  private:
+	bool _renderBodies = true, _renderStats = false;
+	bool _updateBodies = true;
+	kinematics::Simulation _simulation;
+
+	float _frameTimeSeconds;
+	long _updateMicroseconds;
+
+  private:
+	/// Update the simulation according to user input.
+	/// Includes: Toggle for rendering bodies, toggle for updating bodies, setting number of bodies
+	void HandleInput();
+};
