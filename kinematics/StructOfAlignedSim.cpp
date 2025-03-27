8eb88e5f7f7e61978bfd319a7c35ef57e218b1be
Keith Horrocks <KeithJH@users.noreply.github.com>
2025-03-26 18:11:38 -0700

    More bounce check cleanup

--- kinematics/StructOfAlignedSim.cpp
+++ kinematics/StructOfAlignedSim.cpp
@@ -169,21 +169,13 @@ void StructOfAlignedSim::UpdateHelper(const float deltaTime, float *__restrict__
 		bodiesY[i] += bodiesVerticalSpeed[i] * deltaTime;
 
 		// Bounce horizontally
-		if (bodiesX[i] - BODY_RADIUS < 0 && bodiesHorizontalSpeed[i] < 0)
-		{
-			bodiesHorizontalSpeed[i] *= -1;
-		}
-		if (bodiesX[i] + BODY_RADIUS > _width && bodiesHorizontalSpeed[i] > 0)
+		if (BounceCheck(bodiesX[i], bodiesHorizontalSpeed[i], _width))
 		{
 			bodiesHorizontalSpeed[i] *= -1;
 		}
 
 		// Bounce vertically
-		if (bodiesY[i] - BODY_RADIUS < 0 && bodiesVerticalSpeed[i] < 0)
-		{
-			bodiesVerticalSpeed[i] *= -1;
-		}
-		if (bodiesY[i] + BODY_RADIUS > _height && bodiesVerticalSpeed[i] > 0)
+		if (BounceCheck(bodiesY[i], bodiesVerticalSpeed[i], _height))
 		{
 			bodiesVerticalSpeed[i] *= -1;
 		}
