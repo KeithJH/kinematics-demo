acd5682b4ca6bb585dc9359df5e934f54e48f057
Keith Horrocks <KeithJH@users.noreply.github.com>
2025-10-13 18:14:32 -0700

    [false-sharing] Add single thread baseline using local `Point`s

---
+++ videos/false-sharing/01_local_points/bench.cpp
@@ -0,0 +1,33 @@
+#include <array>
+#include <benchmark/benchmark.h>
+#include <cstddef>
+
+constexpr size_t NUM_POINTS = 8;
+constexpr float DELTA_TIME = 1.f/60;
+
+namespace _01_local_points {
+struct Point
+{
+	float position = 1.23f;
+	float velocity = 4.56f;
+
+	void Update() { position += velocity * DELTA_TIME; }
+};
+
+static void Update(benchmark::State& state)
+{
+	std::array<Point, NUM_POINTS> points {};
+
+	benchmark::DoNotOptimize(points);
+	for (auto _ : state)
+	{
+		for (auto &point : points)
+		{
+			point.Update();
+		}
+		benchmark::ClobberMemory();
+	}
+}
+}
+
+BENCHMARK(_01_local_points::Update);
