4e16e5c0e321136ae102d9e8a991633331357c25
Keith Horrocks <KeithJH@users.noreply.github.com>
2025-10-13 18:39:06 -0700

    [false-sharing] demonstrate stride/chunk strategies

--- videos/false-sharing/03_global_atomic_points/bench.cpp
+++ videos/false-sharing/03_global_atomic_points/bench.cpp
@@ -25,28 +25,50 @@ private:
 	std::array<Point, NUM_POINTS_PER_THREAD * MAX_THREADS> _values;
 
 public:
-	void Update(const size_t threadId)
+	void UpdateByStride(const size_t threadId)
 	{
 		for (size_t i = threadId; i < _values.size(); i += MAX_THREADS)
 		{
 			_values[i].Update();
 		}
 	}
+
+	void UpdateByChunk(const size_t threadId)
+	{
+		const size_t chunkStart = threadId * NUM_POINTS_PER_THREAD;
+		for (size_t i = 0; i < NUM_POINTS_PER_THREAD; i++)
+		{
+			_values[i + chunkStart].Update();
+		}
+	}
 };
 
 Points globalPoints {};
 
-static void Update(benchmark::State& state)
+static void UpdateByStride(benchmark::State& state)
 {
 	const size_t threadId = static_cast<size_t>(state.thread_index());
 
 	benchmark::DoNotOptimize(globalPoints);
 	for (auto _ : state)
 	{
-		globalPoints.Update(threadId);
+		globalPoints.UpdateByStride(threadId);
+		benchmark::ClobberMemory();
+	}
+}
+
+static void UpdateByChunk(benchmark::State& state)
+{
+	const size_t threadId = static_cast<size_t>(state.thread_index());
+
+	benchmark::DoNotOptimize(globalPoints);
+	for (auto _ : state)
+	{
+		globalPoints.UpdateByChunk(threadId);
 		benchmark::ClobberMemory();
 	}
 }
 }
 
-BENCHMARK(_03_global_atomic_points::Update)->UseRealTime()->ThreadRange(1, MAX_THREADS);
+BENCHMARK(_03_global_atomic_points::UpdateByStride)->UseRealTime()->ThreadRange(1, MAX_THREADS);
+BENCHMARK(_03_global_atomic_points::UpdateByChunk)->UseRealTime()->ThreadRange(1, MAX_THREADS);
